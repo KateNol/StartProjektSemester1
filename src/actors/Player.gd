68f8cc42ba58d288d90668347e9752cc549a219b
Jannik Rosendahl <jannik-rosendahl@stud.hs-hannover.de>
2021-10-10 21:46:54 +0200

    jump quality of life improvements: 1. allow jumps to be terminated early, 2. input buffer jump, 3. allow jumps for a short period of time even if the player has already lost contact to ground

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -8,11 +8,19 @@ var move_speed : int = 32*6/.8
 var jump_speed : int = -960
 var gravity : int = 2400
 
-var jump_timer : Timer
+var jump_edge_tolerance : float = .1
+var time_since_last_grounded : float
+
+var jump_fall_tolerance : float = .2
+var time_since_last_jump_input : float
 
 
-func input_process():
+var terminate_jump : bool
+
+
+func input_process(delta):
 	direction = Vector2()
+	terminate_jump = false
 	
 	if Input.is_action_pressed("move_left"):
 		direction.x = -1
@@ -20,6 +28,11 @@ func input_process():
 		direction.x = 1
 	if Input.is_action_just_pressed("jump"):
 		direction.y = -1
+		time_since_last_jump_input = 0
+	if Input.is_action_just_released("jump"):
+		terminate_jump = true
+	if direction.y != -1:
+		time_since_last_jump_input += delta
 
 func _ready():
 	velocity = Vector2()
@@ -29,19 +42,33 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	input_process()
+	input_process(delta)
 	
 	velocity.x = direction.x * move_speed
 	velocity.y += gravity * delta
 	
+	if is_on_floor():
+		time_since_last_grounded = 0
+		# jump even if jump input was when player was still in air
+		if time_since_last_jump_input < jump_fall_tolerance:
+			print("air-jump with tolerance time: ", str(time_since_last_jump_input))
+			velocity.y = jump_speed
+	else:
+		time_since_last_grounded += delta
+	# jump, normal
 	if direction.y == -1 and is_on_floor():
-		print("jump")
 		velocity.y = jump_speed
-	
-	
+	# jump even if player is not on floor
+	if direction.y == -1 and time_since_last_grounded < jump_edge_tolerance and not is_on_floor():
+		print("edge-jump with tolerance time: ", str(time_since_last_grounded))
+		velocity.y = jump_speed
+	# terminate jump early
+	if terminate_jump:
+		# 1: velocity reset
+		# if we're still ascending, set reset velocity.
+		# jump only "cancelable" during ascend
+		if velocity.y < 0:
+			velocity.y = 0
 
-
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-func on_timer_timeout():
-	pass
