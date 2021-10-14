73ffd0c5f888524a2a8008b1ecffe2d41b6d246c
Jannik Rosendahl <jannik-rosendahl@stud.hs-hannover.de>
2021-10-14 10:40:38 +0200

    add double jump.

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -19,8 +19,9 @@ var jump_fall_tolerance : float = .2
 var time_since_last_jump_input : float
 var time_since_last_air_jump : float
 
-
 var terminate_jump : bool
+
+var double_jump : bool
 
 
 func input_process(delta):
@@ -61,21 +62,31 @@ func _physics_process(delta):
 	time_since_last_grounded += delta
 	time_since_last_air_jump += delta
 	
+	
+	# jump, normal
+	if direction.y == -1 and is_on_floor():
+		print("normal")
+		velocity.y = jump_speed
+		direction.y = 0
 	if is_on_floor():
+		double_jump = false
 		time_since_last_grounded = 0
 		# jump even if jump input was when player was still in air
 		if (time_since_last_jump_input < jump_fall_tolerance) and (time_since_last_air_jump > 1):
 			print("air-jump")
 			velocity.y = jump_speed
+			direction.y = 0
 			time_since_last_air_jump = 0
-	# jump, normal
-	if direction.y == -1 and is_on_floor():
-		print("normal")
-		velocity.y = jump_speed
 	# jump even if player is not on floor
 	if direction.y == -1 and time_since_last_grounded < jump_edge_tolerance and not is_on_floor():
 		print("edge-jump with tolerance time: ", str(time_since_last_grounded))
 		velocity.y = jump_speed
+		direction.y = 0
+	# double jump
+	if direction.y == -1 and not double_jump and not is_on_floor():
+		velocity.y = jump_speed
+		direction.y = 0
+		double_jump = true
 	# terminate jump early
 	if terminate_jump:
 		# 1: velocity reset
